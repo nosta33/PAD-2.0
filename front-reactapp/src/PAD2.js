a61eefa50fd370a5d16c276d8aad719de25c8b6e
Nosta <hugo.duval33@gmail.com>
2020-03-20 23:20:19 +0100

    fin du calendrier

--- front-reactapp/src/PAD2.js
+++ front-reactapp/src/PAD2.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import Lignes from './lignes.js';
-import ListLivrables from './list-livrables.js';
 
 class PAD2 extends Component {
     render() {
+        const annees = [2020, 2021, 2022, 2023, 2024, 2025];
+        const mois = ["S1", "S2", "S3","S4"];
         return (
         <div id="PAD">
             <div id="top-header">
                 <div id="annees">
-                    annees
+                    {annees.map(annee => (
+                        <div id = {annee} className="annee" key = {annee}>
+                            {annee}
+                        </div>
+                    ))}
                 </div>
 
-                <div id="mois">
-                    mois
+                <div className="mois">
+                    {mois.map(moi => (
+                        <div id = {moi + 2020} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
+                    {mois.map(moi => (
+                        <div id = {moi + 2021} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
+                    {mois.map(moi => (
+                        <div id = {moi + 2022} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
+                    {mois.map(moi => (
+                        <div id = {moi + 2023} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
+                    {mois.map(moi => (
+                        <div id = {moi + 2024} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
+                    {mois.map(moi => (
+                        <div id = {moi + 2025} className="moi" key = {moi}>
+                            {moi}
+                        </div>
+                    ))}
                 </div>
             </div>
             <div id="lignes">
