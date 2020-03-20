662ffcbcec4caca20a47c599b7730b08f34f9b82
Nosta <hugo.duval33@gmail.com>
2020-03-20 14:25:55 +0100

    réorganisation des div

--- front-reactapp/src/PAD2.js
+++ front-reactapp/src/PAD2.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import Lignes from './lignes.js';
 import ListLivrables from './list-livrables.js';
 
 class PAD2 extends Component {
     render() {
-        return (<div id="PAD">
-            <ListLivrables />
+        return (
+        <div id="PAD">
+            <div id="top-header">
+                <div id="annees">
+                    annees
+                </div>
+
+                <div id="mois">
+                    mois
+                </div>
+            </div>
+            <div id="lignes">
+                <Lignes />
+            </div>
         </div>);
+
     }
 }
 
